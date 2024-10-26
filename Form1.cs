6fa3ae670e1351e2a2a5a81808ea6521d762b5ea
nguyenly124 <nguyenthily124da@gmail.com>
2024-10-26 14:29:37 +0700

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,73 +1,46 @@
-﻿using System;
+using System.Security.Cryptography.Xml;
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
-
-namespace form_sinhvien
+namespace form_tra_cuu_diem
 {
+    
     public partial class Form1 : Form
     {
-        List<sinhvien> danhSachSinhVien = new List<sinhvien>();
+
+        List<sinhvien> sv = new List<sinhvien>
+        {
+        new sinhvien { masv = "SV001", hoten = "Nguyen Van A",lop="cnpm6" ,diem = 8 },
+        new sinhvien {  masv = "SV002", hoten = "Nguyen Van B",lop="cnpm8" ,diem = 4 },
+        };
+
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+       
+        private void buttontimkiem_Click(object sender, EventArgs e)
         {
+            string masv = txtmasv.Text;
 
-        }
+            // Tìm sinh viên có mã trùng khớp
+            sinhvien sinhVienTimThay = sv.FirstOrDefault(sv => sv.masv == masv);
 
-        private void toolTip1_Popup(object sender, PopupEventArgs e)
-        {
-
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonthem_Click(object sender, EventArgs e)
-        {
-            string masv = textmasv.Text;
-            string tenSV = texttensv.Text;
-            string lop = textlop.Text;
-            double diem = double.Parse(textdiem.Text);
-            // Lấy thông tin từ TextBox và thêm vào danh sách
-            sinhvien sv = new sinhvien
+            if (sinhVienTimThay != null)
             {
-                masv = masv,
-                hoten = tenSV,
-                lop = lop,
-                diemtb = diem,
-            };
-            danhSachSinhVien.Add(sv);
-
-            // Cập nhật lại DataGridView
-            dataGridView1.DataSource = null;
-            dataGridView1.DataSource = danhSachSinhVien;
+                lable_hienthi.Text = $"Thông tin sinh viên :  {sinhVienTimThay.hoten}        {sinhVienTimThay.lop}      {sinhVienTimThay.diem}";
+            }
+            else
+            {
+                lable_hienthi.Text = "Không tìm thấy sinh viên!";
+            }
         }
-
-        private void buttonsua_Click(object sender, EventArgs e)
-        {
-            
-            // Lấy chỉ số dòng được chọn
-            int rowIndex = dataGridView1.CurrentRow.Index;
-
-            // Lấy thông tin sinh viên từ danh sách
-            sinhvien sv = danhSachSinhVien[rowIndex];
-
-            // Hiển thị thông tin lên TextBox
-            textmasv.Text = sv.masv;
-            texttensv.Text = sv.hoten;
-            textlop.Text = sv.lop;
-            textdiem.Text = sv.diemtb.ToString();
-        }
-
-        private void btnSua_Click(object sender, EventArgs e)
-        {
-            
-        }
-  
     }
+}
+public class sinhvien
+{
+    public string hoten { get; set; }
+    public string masv { get; set; }
+    public string lop { get; set; }
+    public double diem { get; set; }
 }
